458583973ee8fc07e5d95942ccef5f245dc04eb7
Michael May <michael.may@goshoptology.com>
2015-12-08 15:00:07 -0600

    Added notification sounds for admins only;

--- web/app/public/js/services/RoomService.js
+++ web/app/public/js/services/RoomService.js
@@ -102,16 +102,20 @@ define(['angular'], function(angular) {
         });
 
         socket.on('room:update:player:add', function(data) {
-            var audio = new Audio('snd/login.mp3');
-            audio.play();
+            if ($rootScope.user.isAdmin) {
+                var audio = new Audio('snd/login.mp3');
+                audio.play();
+            }
 
             currentRoom.players.push(data.player);
             $rootScope.$broadcast('room:update', currentRoom);
         });
 
         socket.on('room:update:player:remove', function(data) {
-            var audio = new Audio('snd/logout.mp3');
-            audio.play();
+            if ($rootScope.user.isAdmin) {
+                var audio = new Audio('snd/logout.mp3');
+                audio.play();
+            }
 
             var index = getPlayerIndexById(data.player.id, currentRoom);
             currentRoom.players.splice(index, 1);
