b1cc0c0f0584f6ac604bca0585a7428d3dcdf6e7
ivanmayes <ivanmayes@gmail.com>
2015-12-16 15:36:45 -0600

    More login styling

--- web/app/public/js/controllers/LoginCtrl.js
+++ web/app/public/js/controllers/LoginCtrl.js
@@ -3,12 +3,16 @@
 define(function() {
     'use strict';
 
-    function ctrl($scope, $state, $window, UserService) {
+    function ctrl($scope, $state, $window, UserService, $timeout) {
         /*var loggedin = true;
 
         if(loggedin) {
             $state.go('tab.home');
         }*/
+
+        $scope.cat = '';
+        $scope.getSelectedAvatar = getSelectedAvatar;
+        $scope.setSelectedAvatar = setSelectedAvatar;
 
         $scope.signIn = function() {
             $state.go('login');
@@ -17,37 +21,38 @@ define(function() {
         $scope.login = function(user) {
             console.log('logging in');
 
-
-            if (user.name) {
-                if ($scope.selectAvatar) {
-                    var status = (Math.random() > .5) ? 'Nice' : 'Naughty',
-                        avatars = $scope.avatars[status.toLowerCase()],
-                        index = Math.floor(Math.random() * ((avatars.length - 1) - 0 + 1));
-
-                    user.avatar = avatars[index].img;
-                    user.status = status;
-                } else {
-                    user.avatar = $scope.selectedAvatar.img;
-                    user.status = $scope.selectedAvatar.cat;
-                }
-
-                user.token = (user.name + user.status + new Date().getTime()).replace(/\s/g, '_');
-                user.email = user.token + '@goshoptology.com';
-
-                UserService.login(user)
-                    .then(function(userInfo) {
-                        console.log(userInfo);
-                        $state.go('room');
-                    }, function(reason) {
-                            console.log('Failed:', reason);
-                            $scope.errorMsg = reason;
-                        });
-            } else {
-                $scope.errorMsg = 'Please enter your name.';
+            // If no name, give a random one
+            if(!user.name) {
+                user.name = "Anonymous";
             }
+
+            // If no naughty/nice, get nice
+            if($scope.cat) {
+                user.status = $scope.cat;
+                user.avatar = getSelectedAvatar($scope.cat).img;
+            }else{
+                user.status = (Math.random() > .5) ? 'nice' : 'naughty';
+                var avatars = $scope.avatars[user.status.toLowerCase()],
+                    index = Math.floor(Math.random() * ((avatars.length - 1) - 0 + 1));
+
+                user.avatar = avatars[index].img;
+            }
+
+            user.token = (user.name + user.status + new Date().getTime()).replace(/\s/g, '_');
+            user.email = user.token + '@goshoptology.com';
+
+            UserService.login(user)
+                .then(function(userInfo) {
+                    console.log(userInfo);
+                    $state.go('room');
+                }, function(reason) {
+                        console.log('Failed:', reason);
+                        $scope.errorMsg = reason;
+                    });
         };
 
         $scope.selectAvatar = function(avatar, cat) {
+            console.log('gravatar');
             if (avatar.name == 'Gravatar') {
                 console.log('gravatar');
             }
@@ -59,26 +64,70 @@ define(function() {
         $scope.avatars = {
             nice: [
                 {
-                    name: 'Cow',
-                    img: 'cow.jpg'
+                    img: '/img/avatar-01.svg',
+                    selected: true
                 },
                 {
-                    name: 'Moose',
-                    img: 'moose.jpg'
-                }
+                    img: '/img/avatar-02.svg',
+                    selected: false
+                },
+                {
+                    img: '/img/avatar-03.svg',
+                    selected: false
+                },
+                {
+                    img: '/img/avatar-04.svg',
+                    selected: false
+                },
+                {
+                    img: '/img/avatar-05.svg',
+                    selected: false
+                },
             ],
             naughty: [
                 {
-                    name: 'Hitler',
-                    img: 'hitler.jpg'
+                    img: '/img/avatar-06.svg',
+                    selected: true
                 },
                 {
-                    name: 'Stalin',
-                    img: 'stalin.jpg'
-                }
+                    img: '/img/avatar-07.svg',
+                    selected: false
+                },
+                {
+                    img: '/img/avatar-08.svg',
+                    selected: false
+                },
+                {
+                    img: '/img/avatar-09.svg',
+                    selected: false
+                },
+                {
+                    img: '/img/avatar-10.svg',
+                    selected: false
+                },
             ]
         };
-        $scope.selectedAvatar = false;
+
+        function getSelectedAvatar(cat) {
+            var selected = [];
+            angular.forEach($scope.avatars[cat], function(avatar,k) {
+                if (avatar.selected == true) {
+                    selected = avatar;
+                }
+            });
+
+            return selected;
+        }
+
+        function setSelectedAvatar(avatar, cat) {
+            angular.forEach($scope.avatars[cat], function(a,k) {
+                if(avatar == a) {
+                    $scope.avatars[cat][k].selected = true;
+                }else{
+                    $scope.avatars[cat][k].selected = false;
+                }
+            });
+        }
 
 
         // Countdown
@@ -131,9 +180,17 @@ define(function() {
             initializeClock('clockdiv', deadline);
         }, 500);
 
+        $timeout(function() {
+            console.log('focussing');
+            var element = $window.document.getElementById('nameInput');
+            if (element) {
+                element.focus();
+            }
+        });
+
     }
 
-    ctrl.$inject = ['$scope', '$state', '$window', 'UserService'];
+    ctrl.$inject = ['$scope', '$state', '$window', 'UserService', '$timeout'];
     return ctrl;
 
 });
